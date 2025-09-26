4278673a330bd3164c5c2e76f8ca0894aa1b7526
MSI\Admin <trinhbinh191205@hcmut.edu.vn>
2025-09-26 19:19:21 +0700

    update1 for ex1

--- STM32_lab2/Core/Src/main.c
+++ STM32_lab2/Core/Src/main.c
@@ -308,16 +308,16 @@ int counter = 100;
 void HAL_TIM_PeriodElapsedCallback ( TIM_HandleTypeDef * htim ){
 	counter--;
 	if(counter == 50){
-		HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, GPIO_PIN_SET);
-		HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, GPIO_PIN_RESET);
-		display7SEG(2);
+		HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, GPIO_PIN_SET);
+		display7SEG(1);
 	}
 	if( counter <= 0) {
 		counter = 100;
-		HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, GPIO_PIN_RESET);
-		HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, GPIO_PIN_SET);
+		HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, GPIO_PIN_SET);
+		HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, GPIO_PIN_RESET);
+		display7SEG(2);
 		HAL_GPIO_TogglePin ( LED_RED_GPIO_Port , LED_RED_Pin );
-		display7SEG(1);
 	}
 }
 /* USER CODE END 4 */
